ce3feff7e3002a69cc9103410ad8b4749aff7573
ighor.lima <ighor.lima@totvs.com.br>
2024-01-17 20:23:15 -0300

    Meu PortFólio

--- js/script.js
+++ js/script.js
@@ -2,10 +2,17 @@
 //Elementos 
 var Carousel_item = document.getElementsByClassName('carousel-js')
 var contPag = document.getElementsByClassName('contador-pag')
+var Selecoes = document.getElementsByClassName('project')
 //Numerico
 var nAnt = 0
+var nAntSec = 0
+var nDeg = 0
 var nPosic = 0
 var TamItens = 0
+var nCont = 0
+var ocupado = ''
+var status_ = []
+var lDeg = false
 //Array
 var ClassesAuto = ['carousel-js d-none active flex-wrap justify-content-around', 'carousel-js d-flex active flex-wrap justify-content-around' ]
 
@@ -14,6 +21,10 @@ TamItens = Carousel_item.length -1//Recebe a quantidade de Itens existente no ca
 //Seta o tamanho Inicial do Carousel
 contPag[0].innerHTML = '1 de ' + (TamItens + 1).toString()
 contPag[1].innerHTML = '1 de ' + (TamItens + 1).toString()
+
+for(nCont = 0; nCont <= Selecoes.length -1; nCont++){
+	status_[nCont] = ''
+}
 
 //=======================Funções====================
 function mudarPagina(opc){
@@ -50,4 +61,50 @@ function decrementaValor(nValor, nLimite){
 	}
 	
 	return nValor
+}
+
+function transicao(selecao){
+	var x = 0
+
+	if(ocupado == ''){
+		ocupado = 'yes'
+
+		if(selecao == nAntSec && status_[selecao] == 'virado' 
+		 || selecao != nAntSec && status_[selecao] == 'virado'){
+			x = 180
+			lDeg = true
+		}
+
+		var y = setInterval(function (){
+
+			if(lDeg == true){
+				x--
+			}else{
+				x++ 
+			}
+
+			if(x == 91 && lDeg == false){
+				Selecoes[selecao].style.background = 'yellow'
+			}
+			else if(x == 180 && lDeg == false){
+				ocupado = ''
+				nAntSec = selecao
+				status_[selecao] = 'virado'
+				clearInterval(y)
+			}
+
+			if(x == 91 && lDeg == true){
+				Selecoes[selecao].style.background = 'black'
+			}
+			else if(x == 0 && lDeg == true){
+				lDeg = false
+				ocupado = ''
+				status_[selecao] = ''
+				nAntSec = selecao
+				clearInterval(y)
+			}			
+			
+			Selecoes[selecao].style.transform = 'rotateY(' + x.toString() + 'deg)'
+		}, 0.001)		
+	}
 }
